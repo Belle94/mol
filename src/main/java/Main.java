96298a5d34e8bb7dd1abbd68d1835e18f4adfe34
Francesco Bellei <frebelle94@gmail.com>
2015-09-24 15:53:57 +0200

    aggiornato build.gradle e aggiunto main fx

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ * Main class
+ */
+public class Main extends Application {
+    /** the standard method called by main in javafx applications
+     * @param stage of application
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane borderPane = new BorderPane();
+        Scene scene = new Scene(borderPane);
+        stage.show();
+    }
+    /**
+     * Main function
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
