213d5f9dbab86c62c6a99c398d617e545d731e3a
Francesco Bellei <frebelle94@gmail.com>
2015-09-26 13:09:53 +0200

    change qnt to volume, change costructor, implement addGood and removeGood method

--- src/main/java/Bin.java
+++ src/main/java/Bin.java
@@ -1,35 +1,37 @@
+import java.util.ArrayList;
 import java.util.List;
 /**
  * Class implement a Bin.
  */
 public class Bin {
-    private double qntCurrent;
-    private double qntMax;
+    private double volumeCurrent;
+    private double volumeMax;
     private List<Good> goods;
 
-    public Bin(double qntMax, List<Good> goods) {
-        this.qntMax = qntMax;
-        this.goods = goods;
+    public Bin(double volumeMax){
+        this.volumeCurrent = 0;
+        this.volumeMax = volumeMax;
+        goods = new ArrayList<>();
     }
 
-    public double getQntWasted() {
-        return qntMax - qntCurrent;
+    public double getVolumeWasted() {
+        return volumeMax - volumeCurrent;
     }
 
-    public double getQntCurrent() {
-        return qntCurrent;
+    public double getVolumeCurrent() {
+        return volumeCurrent;
     }
 
-    public void setQntCurrent(double qntCurrent) {
-        this.qntCurrent = qntCurrent;
+    public void setVolumeCurrent(double volumeCurrent) {
+        this.volumeCurrent = volumeCurrent;
     }
 
-    public double getQntMax() {
-        return qntMax;
+    public double getVolumeMax() {
+        return volumeMax;
     }
 
-    public void setQntMax(double qntMax) {
-        this.qntMax = qntMax;
+    public void setVolumeMax(double volumeMax) {
+        this.volumeMax = volumeMax;
     }
 
     public List<Good> getGoods() {
@@ -39,4 +41,32 @@ public class Bin {
     public void setGoods(List<Good> goods) {
         this.goods = goods;
     }
+
+    /**
+     * implement addGood method, refresh the volumeCurrent
+     * @param good will be added
+     * @return boolean value, true if the function add the good correctly, else false.
+     */
+    public boolean addGood(Good good){
+        if ( getVolumeWasted() >= (good.getVolume()*good.getQnt()) ){
+            volumeCurrent += good.getVolume()*good.getQnt();
+            goods.add(good);
+            return true;
+        }
+        return false;
+    }
+    /**
+     * implement removeGood method, refresh the volumeCurrent
+     * @param good will be remove
+     * @return boolean value, true if the function remove the good correctly, else false.
+     */
+    public boolean removeGood(Good good){
+        if (goods.contains(good)){
+            volumeCurrent -= good.getQnt()*good.getVolume();
+            goods.remove(good);
+            return true;
+        }
+        return false;
+    }
+
 }
