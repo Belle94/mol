e326b4e76ed683d0831c85f78e23c0e55e06a160
Francesco Bellei <frebelle94@gmail.com>
2015-09-26 15:38:01 +0200

    add Algorithms class and its testing class

---
+++ src/test/main/AlgorithmsTest.java
@@ -0,0 +1,13 @@
+package main;
+
+import junit.framework.TestCase;
+
+/**
+ * AlgorithmsTest class, all testing are implemented below.
+ */
+public class AlgorithmsTest extends TestCase {
+
+    public void testFirstFitDecreasing() throws Exception {
+        
+    }
+}
