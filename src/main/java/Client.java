a4db62815920c8d065326bf2148c8f5072f9d118
Luca Tavernari <tehluke94@gmail.com>
2015-09-26 18:09:44 +0200

    ,

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,5 +1,27 @@
-/**
- * Created by luca on 9/26/15.
- */
 public class Client {
+    private String name;
+    private Integer charge;
+
+    Client(String name, Integer charge) {
+        this.name = name;
+        this.charge = null;
+        setCharge(charge);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCharge() {
+        return this.charge;
+    }
+
+    public void setCharge(Integer charge) {
+        this.charge = charge >= 0 ? charge :
+                this.charge == null ? 0 : charge;
+    }
 }
