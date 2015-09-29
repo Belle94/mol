432c7eebb0d64b6d0fa91429ed31c61806463af9
Francesco Bellei <frebelle94@gmail.com>
2015-09-29 17:57:58 +0200

    dijkstra update, add comments, ovverride equals method and hashcode. Todo: check functions dijkstra

--- src/main/java/AdjacencyList.java
+++ src/main/java/AdjacencyList.java
@@ -2,48 +2,58 @@ import javafx.util.Pair;
 
 import javax.xml.transform.Source;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AdjacencyList {
-    public HashMap<Integer, List<Pair<Integer, Double>>> getG() {
-        return g;
-    }
-
-    public void setG(HashMap<Integer, List<Pair<Integer, Double>>> g) {
-        this.g = g;
-    }
-
     private HashMap<Integer, List<Pair<Integer, Double>>> g;
 
     public AdjacencyList() {
         g = new HashMap<>();
     }
-
     public AdjacencyList(HashMap<Integer, List<Pair<Integer, Double>>> g) {
         this.g = g;
     }
+    public void setGraph(HashMap<Integer, List<Pair<Integer, Double>>> g) {
+        this.g = g;
+    }
 
+    /**
+     * @return the numbers of nodes in the Graph (the size of the graph).
+     */
     public int getNumNodes(){
         int n = g.size();
         return n;
     }
 
+    /**
+     * @return return the Graph
+     */
     public HashMap<Integer,List<Pair<Integer,Double>>> getGraph(){
         return g;
     }
 
+    /** @param source the value of node
+     * @param destination node
+     * @param distance the weight
+     */
     public void addEdge(Integer source, Integer destination, Double distance) {
         List<Pair<Integer, Double>> n = g.containsKey(source) ?
                 g.get(source) : new ArrayList<>();
-        n.add(new Pair<Integer, Double>(destination, distance));
+        n.add(new Pair<>(destination, distance));
         g.put(source, n);
+        if (!g.containsKey(destination))
+            addNode(destination);
     }
 
+    /**
+     * Add new node with an empty List.
+     * @param source the value of the node
+     */
     public void addNode(Integer source){
-        List<Pair<Integer, Double>> n = g.containsKey(source) ?
-                g.get(source) : new ArrayList<>();
-        g.put(source,null);
+        if (!g.containsKey(source)) {
+            g.put(source, new ArrayList<>());
+        }
     }
-
 
     public void addNeighbors(Integer source, List<Pair<Integer, Double>> ns) {
         if (ns != null) {
@@ -61,31 +71,37 @@ public class AdjacencyList {
         if (!g.containsKey(v))
             return null;
 
-        List<Integer> n = new ArrayList<>();
-
-        for (Pair<Integer, Double> p : g.get(v))
-            n.add(p.getKey());
-
-        return n;
+        return g.get(v).stream().map(Pair::getKey).collect(Collectors.toList());
     }
-
+    /**
+     * @param v the input node
+     * @param index of a neighbor list.
+     * @return Distance in the "index" position from the input node.
+     */
     public Double getDistance(Integer v, int index ){
         List<Pair<Integer, Double>> node = g.get(v);
         Pair<Integer, Double> nodep = node.get(index);
         Double distance = nodep.getValue();
         return distance;
     }
-
+    /**
+     * @param v the input node
+     * @param index of a neighbor list that will know the ID
+     * @return ID in the position "index" from the node.
+     */
     public Integer getIdNeighbor(Integer v, int index ){
         List<Pair<Integer, Double>> node = g.get(v);
         Pair<Integer, Double> nodep = node.get(index);
         Integer id = nodep.getKey();
         return id;
     }
-
+    /**
+     * @param v input node
+     * @return numbers of neighbor form the input node.
+     */
     public int getNumNeighbor(Integer v){
         List<Pair<Integer, Double>> neighbor = g.get(v);
-        if(neighbor == null){
+        if(neighbor.isEmpty()){
             return 0;
         }
         return neighbor.size();
@@ -99,11 +115,20 @@ public class AdjacencyList {
     public Set<Integer> getNodes(){
         return g.keySet();
     }
-
+    /**
+     * Dijkstra run with a queue Q.
+     * The queue as initialize only with Source node.
+     * At the first the graph is empty.
+     * The algorithm take the last input node from the queue and
+     * update the value distance with previous distance if there was.
+     * The node is updated if the sum of the previous node plus the
+     * the wight of the edge is less of the current distance value in the node.
+     * @param source the node where Dijkstra start
+     * @return the graph with the shortest path form the node source to each other nodes.
+     */
     public HashMap<Integer, List<Pair<Integer, Double>>> Dijkstra(Integer source){
         HashMap<Integer, List<Pair<Integer, Double>>> pg = new HashMap<>();
         int n = this.getNumNodes();
-        System.out.printf("Nodi: %d\n",n);
         Integer u;
         Double [] dist = new Double[n];
         Integer [] prev = new Integer[n];
@@ -117,8 +142,8 @@ public class AdjacencyList {
         List<Integer> Q = new ArrayList<>();
         Q.add(source);
         while (!Q.isEmpty()){
-            u = Q.get(0);                     //it takes first element on list
-            Q.remove(0);                        //and remove it
+            u = Q.get(0);                     // it takes first element on list
+            Q.remove(0);                        // and remove it
 
             if(dist[u-1] == -1){
                 break;
@@ -147,8 +172,8 @@ public class AdjacencyList {
                 l.add(new Pair<>(i+1, dist[i]));
                 pg.put(prev[i],l);
             }
-            if (ctrl == false) {
-                pg.put(i+1, null);
+            if (!ctrl) {
+                pg.put(i+1, new ArrayList<>());
             }
         }
         return pg;
@@ -160,14 +185,40 @@ public class AdjacencyList {
         if (!(o instanceof AdjacencyList)) return false;
 
         AdjacencyList that = (AdjacencyList) o;
+        if(that.getNodes().size() != this.getNodes().size())
+            return false;
 
-        return g.equals(that.getG());
-
+        for (Integer i : that.getGraph().keySet()) {
+            if (!this.g.containsKey(i))
+                return false;
+            this.g.get(i).sort(AdjacencyList.comparator());
+            that.getGraph().get(i).sort(AdjacencyList.comparator());
+            
+            // if (!this.g.get(i).equals(that.getGraph().get(i))))return true if & only if
+            // Pair in the list are in the same order
+            if (!this.g.get(i).equals(that.getGraph().get(i)))
+                return false;
+            return false;
+            }
+        return true;
     }
 
     @Override
     public int hashCode() {
         return g.hashCode();
     }
-}
 
+    /**
+     * create and implement a new comparator for the node inside the list
+     * @return comparator of 2 Pair
+     */
+    public static Comparator<Pair<Integer,Double>> comparator(){
+        return (o1, o2) -> {
+            if (o1.getKey() < o2.getValue())
+                return -1;
+            if (o1.getKey() > o2.getKey())
+                return 1;
+            return 0;
+        };
+    }
+}
--- src/test/java/AdjacencyListTest.java
+++ src/test/java/AdjacencyListTest.java
@@ -1,9 +1,7 @@
 import javafx.util.Pair;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,27 +13,44 @@ public class AdjacencyListTest {
     public void Dijkstra() throws Exception {
 
         AdjacencyList g = new AdjacencyList();
-        g.addEdge(1,6,14.0);
-        g.addEdge(1,3,9.0);
-        g.addEdge(1,2,7.0);
-        g.addEdge(6,5,9.0);
-        g.addEdge(6,3,2.0);
-        g.addEdge(3,4,11.0);
-        g.addEdge(2,3,10.0);
-        g.addEdge(2,4,15.0);
+        g.addEdge(1, 6,14.0);
+        g.addEdge(1, 3, 9.0);
+        g.addEdge(1, 2, 7.0);
+        g.addEdge(6, 5, 9.0);
+        g.addEdge(6, 3, 2.0);
+        g.addEdge(3, 4, 11.0);
+        g.addEdge(2, 3, 10.0);
+        g.addEdge(2, 4, 15.0);
         g.addEdge(5, 4, 6.0);
         g.addNode(4);
 
         AdjacencyList h = new AdjacencyList();
-        h.addEdge(1,2,7.0);
-        h.addEdge(1,3,9.0);
-        h.addEdge(1,6,14.0);
+        h.addEdge(1, 2, 7.0);
+        h.addEdge(1, 3, 9.0);
+        h.addEdge(1, 6, 14.0);
         h.addNode(2);
-        h.addEdge(3,4,20.0);
+        h.addEdge(3, 4, 20.0);
         h.addNode(4);
         h.addNode(5);
-        h.addEdge(6,5,23.0);
+        h.addEdge(6, 5, 23.0);
 
-        assertEquals(h.getGraph(),g.Dijkstra(1));
+        System.out.println("\nInput Graph");
+        printGraph(g);
+        System.out.println("\nDijkstra Graph:");
+        printGraph(new AdjacencyList(g.Dijkstra(1)));
+        System.out.println("\nExpected Graph");
+        printGraph(h);
+        assertEquals(h.getGraph(), g.Dijkstra(1));
+
+    }
+    private void printGraph(AdjacencyList g){
+
+        for(Integer k : g.getNodes()){
+            System.out.println("Node "+k+"\t hashCode: "+ g.getGraph().get(k).hashCode());
+            for (Pair<Integer,Double> c : g.getGraph().get(k)){
+                System.out.print("(" + c.getKey() + "," + c.getValue() + ") ");
+            }
+            System.out.println();
+        }
     }
 }
