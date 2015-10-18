50ea365c3fe3bb46e219cc506d4e4dd5ec7f7d14
Luca Tavernari <tehluke94@gmail.com>
2015-10-18 17:17:48 +0200

    Write DB without order testing

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -23,28 +23,32 @@ public class DatabaseTest {
 
     public void init() {
         try {
-            for (Integer i = 0; i < 100; i++) {
+            db.openConnection();
+            for (Integer i = 0; i < 2; i++) {
 
                 Client c = new Client("c" + i.toString(), (new Random()).nextInt(90));
                 db.addClient(c);
 
-                for (Integer j = 0; i < new Random().nextInt(4) + 1; j++) {
+                for (Integer j = 0; j < 4; j++) {
                     Order o = new Order();
                     o.setClient(c);
                     o.setDate(new Date());
                     GoodOrder go = new GoodOrder();
                     go.setOrder(o);
-                    for (Integer k = 0; k < new Random().nextInt(4) + 1; k++) {
-                        Good g = new Good(new Random().nextInt(6) + 4, 3, null);
+                    Integer k = 0;
+                    for (; k < (4) + 1; k++) {
+                        Good g = new Good((new Random()).nextInt(6) + 4, 4, "desc" + k.toString());
                         go.setGood(g);
                         db.addGood(g);
                     }
-
+                    go.setQnt(k-1);
                     db.addOrder(o, go);
                 }
             }
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            db.closeConnection();
         }
     }
 
@@ -59,8 +63,30 @@ public class DatabaseTest {
     }
     @Test
     public void outputDatabaseTest() throws Exception{
+        db.openConnection();
+        System.out.println("CLIENTS:");
+        for (Client c : db.getAllClients()) {
+            System.out.println("------");
+            System.out.println(c.toString());
+            System.out.println("------");
+        }
 
+        /*System.out.println("\nORDERS:");
+        for (Order o : db.getAllOrders()) {
+            System.out.println("------");
+            System.out.println(o.toString());
+            System.out.println("------");
+        }*/
+
+        System.out.println("\nGOOD_ORDER");
+        for (GoodOrder go : db.getAllGoodOrders()) {
+            System.out.println("------");
+            System.out.println(go.toString());
+            System.out.println("------");
+        }
+        db.closeConnection();
     }
+
     @Test
     public void deleteDatabaseTest() throws Exception{
 
