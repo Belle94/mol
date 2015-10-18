d29211f4e6db31afbf7e46b1ee73234c20ef37fc
Luca Tavernari <tehluke94@gmail.com>
2015-10-18 17:16:03 +0200

    Add toString method and constructor

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -8,22 +8,40 @@ import java.util.Date;
 /**
  * Class implement an order.
  */
-@DatabaseTable(tableName = "order")
+@DatabaseTable(tableName = "orders")
 public class Order {
     public final static String CLIENT_FIELD_NAME = "client_id";
     public final static String DATE_FIELD_NAME = "date";
+    public final static String ITINERARY_FIELD_NAME = "it";
 
     @DatabaseField (generatedId = true)
     private Integer id;
-    @DatabaseField(foreign = true, foreignAutoRefresh = true, columnName = CLIENT_FIELD_NAME)
+    @DatabaseField(foreign = true, foreignAutoRefresh = true,
+            columnName = CLIENT_FIELD_NAME)
     private Client client;
     @DatabaseField (canBeNull = false, columnName = DATE_FIELD_NAME)
     private Date date;
+    @DatabaseField(foreign = true, foreignAutoRefresh = true,
+            columnName = ITINERARY_FIELD_NAME)
+    private Itinerary itinerary;
     @ForeignCollectionField
     private ForeignCollection<GoodOrder> goodOrders;
 
     public Order() {
         // all persisted classes must define a no-arg constructor with at least package visibility
+    }
+
+    public Order (Integer id, Client c, Date d, Itinerary i) {
+        this.id = id;
+        this.client = c;
+        this.date = d;
+        this.itinerary = i;
+    }
+
+    public Order (Client c, Date d, Itinerary i) {
+        this.client = c;
+        this.date = d;
+        this.itinerary = i;
     }
 
     public Integer getId() {
@@ -56,5 +74,21 @@ public class Order {
 
     public void setGoodOrders(ForeignCollection<GoodOrder> goodOrders) {
         this.goodOrders = goodOrders;
+    }
+
+    public Itinerary getItinerary() {
+        return itinerary;
+    }
+
+    public void setItinerary(Itinerary itinerary) {
+        this.itinerary = itinerary;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                "\nClient ID: " + client.getId() +
+                "\nDate: " + date.toString() +
+                "\nItinerary: " + itinerary.getId();
     }
 }
