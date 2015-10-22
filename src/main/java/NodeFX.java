9e1e14151b9b8bbc36796dbe0e2feb5b515718c3
Bruno Ghion <ghion.g94@gmail.com>
2015-10-22 11:57:16 +0200

    NodeFX.java

---
+++ src/main/java/NodeFX.java
@@ -0,0 +1,12 @@
+/**
+ * Created by bruce on 22/10/15.
+ */
+import javafx.scene.paint.Color;
+
+public class NodeFX {
+    public int px, py;
+    public int dim_rag, area_non_amm, dist_nodi;
+    public int max_nodi;
+    public Color colore;
+
+}
