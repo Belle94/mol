1bdc25a5805e8e4ecd318b6cd6b445e5bf07434d
Bruno Ghion <ghion.g94@gmail.com>
2015-11-01 20:08:08 +0100

    litle fix

--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -7,8 +7,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCombination;
@@ -533,13 +531,7 @@ public class Gui {
         searchPane.setVisible(false);
     }
 
-    private ScrollPane createSetScrollPane(TableView tableView){
-        ScrollPane scrollPane = new ScrollPane();
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-        scrollPane.setContent(tableView);
-        return scrollPane;
-    }
+
 
     private void initMainPane(){
         mainPane = new StackPane();
